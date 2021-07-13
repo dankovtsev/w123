4ba260a155a98cd9a75026624d138dd4df56f818
dankovtsev <dankovtsev_a@bk.ru>
2021-07-13 20:48:19 +0300

    version 2

--- user_table.php
+++ user_table.php
@@ -1,4 +1,3 @@
-<link href="input.css" rel="stylesheet">
 <link rel = "stylesheet" href = "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 <?php
 if($_COOKIE['user'] ==''){    //проверка на зайцев
@@ -6,17 +5,17 @@ if($_COOKIE['user'] ==''){    //проверка на зайцев
 }
 include_once("table_data.php");
 ?>
+
 <form action="appraisal_table.php" method="post" name="drop_down_box">
-<input type="text" id="appr" class="form-control" name='num' placeholder="номер строки" required autofocus >
-<select name="menu" size="1">
-<option value="first">*</option>
-<option selected="selected" value="second">**</option>
-<option value="third"> *** </option>
-</select>
-<button class="btn btn-lg btn-primary btn-block" type="submit">добавить</button>
+  <input type="text" id="appr" style="max-width: 330; width: 280;" name='num' placeholder="номер строки" required autofocus >
+    <select name="menu" size="1">
+    <option value="first">*</option>
+    <option selected="selected" value="second">**</option>
+    <option value="third"> *** </option>
+   </select>
+  <button type="button" class="btn btn-lg btn-primary" style="padding-top: 1px; padding-bottom: 2px;"disabled>Добавить</button>
 </form>
 <div><hr>
 <p> Просто пользователь 1 -  проверка роли </P>
 </div><hr>
 <p> Если желаете покинуть нас, то рядом <a href="/exit.php">выход</a> </p>
-
